dd8f5423ba85efa5fd4f0d1ced1efec6938c0b9a
Guillermo Moreno <jmemox@gmail.com>
2021-02-16 12:24:39 -0600

    Create Articles with Rails Scaffold

---
+++ db/migrate/20210216182210_create_articles.rb
@@ -0,0 +1,11 @@
+class CreateArticles < ActiveRecord::Migration[6.1]
+  def change
+    create_table :articles do |t|
+      t.text :name
+      t.text :descripcion
+      t.bigint :amount_cents
+
+      t.timestamps
+    end
+  end
+end
